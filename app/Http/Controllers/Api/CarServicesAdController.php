bef15d1a09a85793218ab2d2287989bc6dff4a04
Abdelrhman0101 <abdalrhman.software.eg@gmail.com>
2025-09-04 03:23:27 +0300

    11: Car Services ( Ad Detailes - Filters - offer box ) V1.5

--- app/Http/Controllers/Api/CarServicesAdController.php
+++ app/Http/Controllers/Api/CarServicesAdController.php
@@ -138,11 +138,11 @@ class CarServicesAdController extends Controller
         // 3. تحديد حالة الإعلان بناءً على الإعداد
         if ($isManualApprovalActive) {
             // إذا كانت الموافقة اليدوية مفعلة، الإعلان ينتظر المراجعة
-            $data['add_status'] = 'pending';
+            $data['add_status'] = 'Pending';
             $data['admin_approved'] = false;
         } else {
             // إذا كانت الموافقة اليدوية معطلة، الإعلان يتم نشره تلقائيًا
-            $data['add_status'] = 'active';
+            $data['add_status'] = 'Valid';
             $data['admin_approved'] = true;
         }
 
